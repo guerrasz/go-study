fc3321fb1cc0464f87a8f0ff1c161cb68aa1716f
guerrasz <lucasgasardo@gmail.com>
2026-01-21 15:20:05 -0300

    feat: create file detailing go operators

---
+++ 1_basics/8_operators/operators.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+	//& Logical operators
+	//. ! (Logical not)
+	//. && (Logical and)
+	//. || (Logical or)
+
+	//& Bitwise operators
+	//. & (Bitwise and)
+	//. | (Bitwise or)
+	//. ^ (Bitwise xor)
+	//. &^ (Bitwise and not)
+	//. << (Left shift)
+	//. >> (Right shift)
+
+	//& Comparison operators
+	//. == (Equal)
+	//. != (Not equal)
+	//. < (Less than)
+	//. > (Greater than)
+	//. <= (Less than or equal to)
+	//. >= (Greater than or equal to)
+
+}
