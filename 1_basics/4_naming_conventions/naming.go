8a366baf70b777b1f8ba66d9dc40797075a65f89
guerrasz <lucasgasardo@gmail.com>
2026-01-21 14:13:27 -0300

    feat: add naming conventions file with examples

---
+++ 1_basics/4_naming_conventions/naming.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	FirstName string
+	Age       int
+}
+
+func main() {
+	//& PascalCase
+	// Structs, interfaces, enums
+
+	//& snake_case
+	// files
+
+	//& UPPERCASE
+	// Constants
+
+	//& mixedCase
+	// Variables
+
+	//& singleword
+	// folder and package names
+
+	const MAXRETRIES = 5
+
+	var employeeID = 2003
+
+	fmt.Println(employeeID)
+}
