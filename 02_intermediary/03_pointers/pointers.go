4501ca26e4a403f746f940176a61cd9c45fc3401
guerrasz <lucasgasardo@gmail.com>
2026-01-26 11:13:24 -0300

    feat: finish examples of pointers

--- 02_intermediary/03_pointers/pointers.go
+++ 02_intermediary/03_pointers/pointers.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	// var ptr *int
+	// zero value of a pointer is nil
 
 	var ptr *int
 	var a int = 10
@@ -13,7 +14,16 @@ func main() {
 	fmt.Printf("Address of a: %p\n", &a)
 	// also address because v of pointer is the address
 	fmt.Printf("Value of ptr: %v\n", ptr)
-	// dereferecing the pointer to get the value 
+	// dereferecing the pointer to get the value
 	fmt.Printf("Value of *ptr: %d\n", *ptr)
 
+	// value is changed because we change the address
+	modifyValue(ptr)
+	fmt.Printf("Value of *ptr: %d\n", *ptr)
+
+}
+
+// increments the value by 1
+func modifyValue(ptr *int) {
+	*ptr++
 }
