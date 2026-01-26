cdaeb5605ef7afb978d6e218aa6a5b3169335ca6
guerrasz <lucasgasardo@gmail.com>
2026-01-26 19:49:12 -0300

    feat: update bash script to require less typing and append main function

--- create_package.sh
+++ create_package.sh
@@ -11,5 +11,25 @@ fi
 directory=$1
 package=$2
 
-mkdir -p $directory/$package
-touch $directory/$package/$package.go
+# find dir that starts with $1
+parent=$(find . -maxdepth 1 -name "${directory}_*" | head -n 1)
+
+if [ -z "$parent" ]; then
+    echo "Error: Directory prefix '${directory}' not found."
+    exit 1
+fi
+
+# find highest number in directory
+highest=$(ls $parent | sort | tail -1 | cut -c 1 -c 2)
+
+# add 1 to highest
+current=$((10#$highest + 1))
+
+# create new dir with name $2
+mkdir -p ${parent}/${current}_${package}
+
+# create .go file with name $2
+touch ${parent}/${current}_${package}/${package}.go
+
+# add main function to .go file
+printf "package main\n\nfunc main() {\n\n}" > ${parent}/${current}_${package}/${package}.go
